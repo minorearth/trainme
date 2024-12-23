4d3c4fd8e2dbc2c01f6529d1ecbc40b7498aa062
romaninformatika <romaninformatika@gmail.com>
2024-12-23 20:50:48 +0300

    trying find bug6

---
+++ src/components/congratulation/congratAnimation.js
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import { Box } from "@mui/material";
+import { useEffect } from "react";
+import Animation from "./animation.json";
+import Lottie from "lottie-react";
+
+const CongratAnimation = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <Lottie
+        style={{ height: "700px", width: "700px" }}
+        animationData={Animation}
+        loop={true}
+      ></Lottie>
+    </Box>
+  );
+};
+
+export default CongratAnimation;
---
+++ src/components/test/congrat.js
@@ -0,0 +1,37 @@
+"use client";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CongratAnimation from "@/components/congratulation/congratAnimation";
+import ReplayIcon from "@mui/icons-material/Replay";
+import { useTheme } from "@mui/material/styles";
+
+const Congrat = ({ action }) => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: window.innerHeight,
+        alignItems: "center",
+        justifyContent: "center",
+        transition: "padding 5s",
+        backgroundColor: theme.palette.background.default,
+      }}
+    >
+      <CongratAnimation />
+
+      <Button
+        sx={{ mt: 3, mb: 3 }}
+        variant="outlined"
+        aria-label="repeat"
+        onClick={() => action(false)}
+        endIcon={<ReplayIcon />}
+      >
+        {"Вернуться к курсу"}
+      </Button>
+    </Box>
+  );
+};
+
+export default Congrat;
