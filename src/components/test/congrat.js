39a56670aa64cc14c1e0d490583be5fe241c2a2b
romaninformatika <romaninformatika@gmail.com>
2024-12-23 20:55:29 +0300

    trying find bug8

--- src/components/test/congrat.js
+++ src/components/test/congrat.js
@@ -1,7 +1,7 @@
 "use client";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import CongratAnimation from "@/components/congratulation/congratAnimation";
+// import CongratAnimation from "@/components/congratulation/congratAnimation";
 import ReplayIcon from "@mui/icons-material/Replay";
 import { useTheme } from "@mui/material/styles";
 
