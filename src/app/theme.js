3b5a6ddaf41a20c99d35aae2ef001da91171d866
romaninformatika <romaninformatika@gmail.com>
2025-01-29 17:59:57 +0300

    network font

--- src/app/theme.js
+++ src/app/theme.js
@@ -1,12 +1,20 @@
 "use client";
 import { createTheme } from "@mui/material/styles";
 // import Monaco from "/Monaco.ttf";
+import localFont from "next/font/local";
+
+const myFont = localFont({
+  src: "./Monaco.ttf",
+});
+
+// Font files can be colocated inside of `pages`
 
 // https://zenoo.github.io/mui-theme-creator/
 export const darkTheme = createTheme({
   typography: {
     body1: {
-      fontFamily: "Monaco",
+      // fontFamily: myFont,
+      fontFamily: myFont.style.fontFamily,
     },
   },
 
@@ -18,18 +26,18 @@ export const darkTheme = createTheme({
     dark: true,
   },
   components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-        @font-face {
-          font-family: 'Monaco';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: local('Monaco'), local('Monaco-Regular'), url('Monaco.ttf') format('ttf');
-          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-        }
-      `,
-    },
+    // MuiCssBaseline: {
+    //   styleOverrides: `
+    //     @font-face {
+    //       font-family: 'Monaco';
+    //       font-style: normal;
+    //       font-display: swap;
+    //       font-weight: 400;
+    //       src: local('Monaco'), local('Monaco-Regular'), url('/Monaco.ttf') format('ttf');
+    //       unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+    //     }
+    //   `,
+    // },
     MuiInputBase: {
       styleOverrides: {
         root: {
