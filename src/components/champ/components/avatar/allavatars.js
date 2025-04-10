5db36fa9cbb42039c66e9d56fa2c32b89e6809bc
romaninformatika <romaninformatika@gmail.com>
2025-04-10 20:23:48 +0300

    new avatars

--- src/components/champ/components/avatar/allavatars.js
+++ src/components/champ/components/avatar/allavatars.js
@@ -1,6 +1,9 @@
 import Avatar0 from "./avatars/avatar0";
 import Avatar1 from "./avatars/avatar1";
 import Avatar2 from "./avatars/avatar2";
+import Avatar3 from "./avatars/avatar3";
+import Avatar4 from "./avatars/avatar4";
+import Avatar5 from "./avatars/avatar5";
 
 // https://www.svgrepo.com/svg/454517/animal-dog-domestic-3
 
@@ -8,4 +11,7 @@ export const avatars = [
   <Avatar0 key={"0"} />,
   <Avatar1 key={"1"} />,
   <Avatar2 key={"2"} />,
+  <Avatar3 key={"3"} />,
+  <Avatar4 key={"4"} />,
+  <Avatar5 key={"5"} />,
 ];
