e2d78e3533e3c334b81ecafdd8fc4495bc6ca1b8
romaninformatika <romaninformatika@gmail.com>
2025-04-16 18:57:53 +0300

    network issue

---
+++ src/app/api/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
+export const revalidate = 0; //revalidate api every 0 second
+//https://stackoverflow.com/questions/76356803/data-not-updating-when-deployed-nextjs13-app-on-vercel-despite-using-cache-no
+
+// import { createIndexAPI } from "./domain";
+
+export async function GET() {
+  // await createIndexAPI();
+  const res = { job: "done" };
+  return NextResponse.json(res);
+}
