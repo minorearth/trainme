ca98a4149c052ff93d0f6ef71a294024ff800e62
romaninformatika <romaninformatika@gmail.com>
2025-05-04 19:25:18 +0300

    joinGroup WIP

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,12 @@ import { cookies } from "next/headers";
 export function middleware(request) {
   const session = request.cookies.get("session");
 
-  if (!session && request.nextUrl.pathname.startsWith("/chapters")) {
+  if (
+    !session &&
+    (request.nextUrl.pathname.startsWith("/chapters") ||
+      request.nextUrl.pathname.startsWith("/groups") ||
+      request.nextUrl.pathname.startsWith("/joingroup"))
+  ) {
     return Response.redirect(new URL("/", request.url));
   }
   // if (
