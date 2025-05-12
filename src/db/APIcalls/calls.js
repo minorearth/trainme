a8b01d1b4f17b09f5807cd52cf1d4c7fad1e8164
romaninformatika <romaninformatika@gmail.com>
2025-05-12 23:09:28 +0300

    bugfix

--- src/db/APIcalls/calls.js
+++ src/db/APIcalls/calls.js
@@ -1,4 +1,4 @@
-setInterval(() => {
+export const wakeUp = () => {
   fetch("/api/wakeup", {
     method: "POST",
     headers: {
@@ -8,25 +8,32 @@ setInterval(() => {
   })
     .then((res) => console.log("Pinged API to keep awake"))
     .catch((err) => console.error("Ping failed", err));
+};
+
+setInterval(() => {
+  //  wakeUp
 }, 30000); // каждые 45 секунд
 
 export const setDataFetch = async (data) => {
-  try {
-    const response = await fetch("/api/setmeta", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    // if (result.res == "error") {
-    //   // throw new Error("Network response was not ok");
-    // }
-    return result.res;
-  } catch (error) {
-    return "error";
-  }
+  wakeUp();
+  setTimeout(async () => {
+    try {
+      const response = await fetch("/api/setmeta", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      // if (result.res == "error") {
+      //   // throw new Error("Network response was not ok");
+      // }
+      return result.res;
+    } catch (error) {
+      return "error";
+    }
+  }, 1000);
 };
 
 export const getDataFetch = async (data) => {
