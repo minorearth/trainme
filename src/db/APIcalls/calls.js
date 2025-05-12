08d88eef0f92ddacfd50a66b54b1206ecb51cecc
romaninformatika <romaninformatika@gmail.com>
2025-05-12 22:58:09 +0300

    joinGroup WIP

--- src/db/APIcalls/calls.js
+++ src/db/APIcalls/calls.js
@@ -25,8 +25,7 @@ export const setDataFetch = async (data) => {
     // }
     return result.res;
   } catch (error) {
-    console.log("erroroooo");
-    console.error("Error:", error);
+    return "error";
   }
 };
 
