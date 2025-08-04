be451257203e92c721f8f47d88dbb820f38d02b4
romaninformatika <romaninformatika@gmail.com>
2025-08-04 22:07:08 +0300

    payment api

--- src/app/api/payments/route.ts
+++ src/app/api/payments/route.ts
@@ -1,5 +1,8 @@
 // import { NextResponse } from "next/server";
 
+import { NextResponse } from "next/server";
+import { getNextErrorResponse } from "tpconst/errorHandlers";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 1; //revalidate api every 1 second
 //https://stackoverflow.com/questions/76356803/data-not-updating-when-deployed-nextjs13-app-on-vercel-despite-using-cache-no
@@ -11,9 +14,10 @@ interface reqData {
 export async function POST(request: Request) {
   try {
     const reqData: reqData = await request.json();
-    console.log("2ok2", reqData);
+    console.log("2ok2");
     console.log("payment", reqData);
+    return NextResponse.json(reqData, { status: 200 });
   } catch (error) {
-    // return NextResponse.json(getNextErrorResponse(error), { status: 500 });
+    return NextResponse.json(getNextErrorResponse(error), { status: 500 });
   }
 }
