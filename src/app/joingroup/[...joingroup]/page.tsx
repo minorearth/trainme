f0da5ee031b094864c5bf03348c0cfd7b0df340b
romaninformatika <romaninformatika@gmail.com>
2026-03-12 12:59:16 +0300

    bugfixes course WIP

--- src/app/joingroup/[...joingroup]/page.tsx
+++ src/app/joingroup/[...joingroup]/page.tsx
@@ -3,6 +3,10 @@ import { ThemeProvider } from "@mui/material/styles";
 import { useCustomTheme } from "@/app/theme";
 import { CssBaseline } from "@mui/material";
 import Joingroup from "@/components/manager/joinGroup/joingroup";
+import { useEffect } from "react";
+import { startUidMonitor } from "@/tpconst/src";
+import user from "@/auth/store/user";
+import { logout } from "@/globals/next/session";
 
 interface PageProps {
   params: {
@@ -11,6 +15,12 @@ interface PageProps {
 }
 export default function Page({ params }: PageProps) {
   const { customTheme } = useCustomTheme();
+  useEffect(() => {
+    startUidMonitor(
+      (id) => user.setUserid(id),
+      () => logout(),
+    );
+  }, []);
   return (
     <>
       <ThemeProvider theme={customTheme}>
